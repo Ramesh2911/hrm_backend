938154ecc4fb4dcd848e9dab8bd15c3d781d0cd8
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-12-16 18:48:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-   origin: ["http://localhost:3000"],
+   origin: ["https://radianceit-uk.vercel.app"],
    methods: ['GET', 'POST', 'PUT', "DELETE"],
    credentials: true
 }));
